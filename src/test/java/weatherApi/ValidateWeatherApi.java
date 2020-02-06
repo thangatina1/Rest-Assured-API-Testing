08fbee165c526971ca1c6da4128ffd0872d4f849
thangatina1 <montfort1A>
2020-02-05 21:10:56 -0500

    Removed the Cucumber related files and dependencies

--- src/test/java/weatherApi/ValidateWeatherApi.java
+++ src/test/java/weatherApi/ValidateWeatherApi.java
@@ -2,6 +2,7 @@ package weatherApi;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
@@ -18,7 +19,7 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.ResponseSpecification;
 
 public class ValidateWeatherApi {
-	
+
 	@BeforeClass
 	public void initPath() {
 
@@ -30,59 +31,66 @@ public class ValidateWeatherApi {
 	public void validateResponseCode() {
 		given().when().
 
-				get("/data/2.5/weather?zip=94040,us&appid=651587654bcd0392c12915b211ad8efa").
-				then()
+				get("/data/2.5/weather?zip=94040,us&appid=651587654bcd0392c12915b211ad8efa")
+				.then()
 				.statusCode(200);
-		
+
 	}
 
 	// Passing Parameters to GET Request
 	@Test
 	public void passQueryParametersToGetRequest() {
-		
+
 		given().
-		params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").
-		when().
-		get("/data/2.5/weather").then().body("cod", equalTo(200));
-		
-		
+				params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather").then().body("cod", equalTo(200));
+
+
 	}
 
 	// Passing Parameters to GET Request
 	@Test
 	public void validateTemperature() {
-		
-		int minTemp = given().
-		params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").
-		when().
-		get("/data/2.5/weather").
-		then().extract().
-        path("main.temp_min");
-		
-		int maxTemp = given().
-				params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").
-				when().
-				get("/data/2.5/weather").
-				then().extract().
-		        path("main.temp_max");
-		
-		int temp = given().
-				params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").
-				when().
-				get("/data/2.5/weather").
-				then().extract().
-		        path("main.temp");
-		
+
+		float minTemp = given()
+				.params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.extract()
+				.path("main.temp_min");
+
+		float maxTemp = given()
+				.params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.extract()
+				.path("main.temp_max");
+
+		float temp = given()
+				.params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.extract()
+				.path("main.temp");
+
 		assertTrue(minTemp <= temp && temp <= maxTemp);
-		
-		
+
+
 	}
+
 	// PathParameter to read a JSON file
 	@Test
 	public void useSinglePathParameter() {
 
-		String responseBody = given().pathParam("suffixName", "monarchs").when()
-				.get("http://mysafeinfo.com/api/data?list=english{suffixName}&format=json").getBody().asString();
+		String responseBody = given()
+				.pathParam("suffixName", "monarchs")
+				.when()
+				.get("http://mysafeinfo.com/api/data?list=english{suffixName}&format=json")
+				.getBody().asString();
 		// System.out.println("Response Body is: " + responseBody);
 	}
 
@@ -90,89 +98,102 @@ public class ValidateWeatherApi {
 	@Test
 	public void negativeTest() {
 
-		given().params("zip", "94040,AAA", "appid", "651587654bcd0392c12915b211ad8efa").
-		when().
-		get("/data/2.5/weather").then().body("message", equalTo("city not found"), "cod", equalTo("404"));
+		given().params("zip", "94040,AAA", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.body("message", equalTo("city not found"), "cod", equalTo("404"));
 	}
+
 	@Test
 	// Validate if the format is JSON
 	public void checkResponseContentTypeJson() {
-				
-		given().params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").when().
-		get("/data/2.5/weather").
-		then().
-		assertThat().
-		contentType("application/json");
+
+		given().params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.assertThat()
+				.contentType("application/json");
 	}
-	
+
 	//A Key is available in the Response
 	@Test
 	public void isKeyAvailable() {
-		
-		given().params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").when().
-		get("/data/2.5/weather").
-		then().
-			assertThat().
-			body("$", hasKey("name")).
-			body("$", not(hasKey("IamInvisible")));
+
+		given().params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.assertThat()
+				.body("$", hasKey("name"))
+				.body("$", not(hasKey("IamInvisible")));
 
 	}
+
 	//Data Providers
 	// Returning a String Array. Data Providers can return maps too
 	@DataProvider(name = "city")
-	public String[][] cityData() {
-		return new String[][] {
-				{ "94040,us", "Mountain View" },
-				{ "03820,us", "Manchester" },
-				{ "75024,us", "Frisco" }
+	public Object[][] cityData() {
+		return new String[][]{
+				{"94040,us", "Mountain View"},
+				{"03820,us", "Dover"},
+				{"75024,us", "Plano"}
 		};
 	}
-		@Test(dataProvider = "city")
-		public void validateWithDataProviders(String zipCode, String cityName) {
-			
-			given().
-			params("zip", zipCode, "appid", "651587654bcd0392c12915b211ad8efa").
-			when().
-			get("/data/2.5/weather").
-			then().
-			body("name",equalTo(cityName));
-			
-		
-		}
-		@Test
-		public void responseTimeValidation() {
-			
-			given().
-			params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").
-			when().
-			get("/data/2.5/weather").
-			then().
-			assertThat().
-			time(lessThan(1000L),TimeUnit.MILLISECONDS);
-		}
-		
-		ResponseSpecification respSpec;
-		@BeforeClass
-		public void createResponseSpecification() {
-			
-			respSpec = new ResponseSpecBuilder().
-					expectStatusCode(200).
-					expectContentType(ContentType.JSON).
-					expectBody("name", equalTo("Mountain View")).
-					build();		
-		}
-		
-		@Test
-		public void useResponseSpecification() {
-			given().
-			params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa").
-			when().
-			get("/data/2.5/weather").then().		
-			
-				spec(respSpec).
-				and().
-				body("cod",equalTo(200));
-		}
+
+	@Test(dataProvider = "city")
+	public void validateWithDataProviders(String zipCode, String cityName) {
+
+		String city = given()
+				.params("zip", zipCode, "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.extract()
+				.path("name");
+
+		System.out.println(city);
+
+		assertEquals(city, cityName);
+
+	}
+
+	@Test
+	public void responseTimeValidation() {
+
+		given()
+				.params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.assertThat()
+				.time(lessThan(1000L), TimeUnit.MILLISECONDS);
+	}
+
+	ResponseSpecification respSpec;
+
+	@BeforeClass
+	public void createResponseSpecification() {
+
+		respSpec = new ResponseSpecBuilder()
+				.expectStatusCode(200)
+				.expectContentType(ContentType.JSON)
+				.expectBody("name", equalTo("Mountain View")).
+						build();
+	}
+
+	@Test
+	public void useResponseSpecification() {
+		given().
+				params("zip", "94040,us", "appid", "651587654bcd0392c12915b211ad8efa")
+				.when()
+				.get("/data/2.5/weather")
+				.then()
+				.spec(respSpec)
+				.and()
+				.body("cod", equalTo(200));
+	}
 
 }
 	
